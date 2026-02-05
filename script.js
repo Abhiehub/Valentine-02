98828a0f05a09aeb2921574b4d7914cd22b048b8
Abhiehub <Abhi121prakash@gmail.com>
2026-02-06 01:09:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,23 +13,6 @@ function openEnvelope() {
 
 // Yes button action
 document.getElementById("yesBtn").addEventListener("click", function() {
-    document.getElementById("popup").classList.remove("hidden");
+    document.getElementById("finalMessage").classList.remove("hidden");
     document.getElementById("message").classList.add("hidden");
-});
-
-// No button action
-let noBtnClickCount = 0;
-document.getElementById("noBtn").addEventListener("click", function() {
-    noBtnClickCount++;
-    this.style.pointerEvents = "none"; // Disable clicking
-    // Change button behavior after each click (e.g., change path, color, etc.)
-    if (noBtnClickCount === 1) {
-        this.innerText = "No... Really?";
-        this.style.backgroundColor = "#ffcc00";
-    } else if (noBtnClickCount === 2) {
-        this.innerText = "You're really sure?";
-        this.style.backgroundColor = "#d32f2f";
-    } else {
-        this.innerText = "Okay, I get it!";
-    }
 });
